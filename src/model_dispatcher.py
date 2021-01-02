d1ee1d1b7e5755bf2d22f2a7f1700525dec940e6
Domagoj Krivosic <domagoj_70@hotmail.com>
2021-01-02 20:20:36 +0100

    Add XGBRegressor

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -1,6 +1,8 @@
 from sklearn import linear_model
+import xgboost as xgb
 
 
 models = {
-    'sgd_regressor': linear_model.SGDRegressor(loss='squared_loss', max_iter=1000)
+    'sgd_regressor': linear_model.SGDRegressor(loss='squared_loss', max_iter=1000),
+    'xgboost': xgb.XGBRegressor(n_jobs=-1)
 }
