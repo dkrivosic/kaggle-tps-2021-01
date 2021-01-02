0ab7c5026e1f4f316dc5f0cbd4b29f1bfc48605d
Domagoj Krivosic <domagoj_70@hotmail.com>
2021-01-02 18:30:38 +0100

    Implement inference script

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 TRAINING_FILE = 'input/train_folds.csv'
 TEST_FILE = 'input/test.csv'
+SUBMISSION_FILE = 'input/submission.csv'
 MODEL_OUTPUT = 'models'
 KFOLD_SEED = 70
---
+++ src/inference.py
@@ -0,0 +1,32 @@
+import argparse
+import pickle
+
+import pandas as pd
+
+import config
+
+
+def run(model):
+    with open(model, 'rb') as f:
+        reg_model = pickle.load(f)
+
+    df = pd.read_csv(config.TEST_FILE)
+    x_test = df.drop('id', axis=1).values
+
+    print(x_test.shape)
+
+    y_pred = reg_model.predict(x_test)
+    
+    df['target'] = y_pred
+    feature_columns = [column for column in df.columns
+                       if column not in ['id', 'target']]
+    df = df.drop(feature_columns, axis=1)
+    df.to_csv(config.SUBMISSION_FILE, index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, required=True)
+    args = parser.parse_args()
+
+    run(args.model)
