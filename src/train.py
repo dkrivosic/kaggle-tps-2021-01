ac02eb9f58a0baed01d1a38d43b78fbc941979f5
Domagoj Krivosic <domagoj_70@hotmail.com>
2021-01-02 20:23:03 +0100

    Train the model on full training data
    After training the model k times and evaluating it on a fold
    each time, train it again on the whole training dataset.

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 import pickle
 
 import pandas as pd
+import numpy as np
 from sklearn.metrics import mean_squared_error
 
 import config
@@ -25,18 +26,24 @@ def run(model, fold):
     training_time = time.time() - start_time
     training_time = str(timedelta(seconds=training_time))
 
-    df_val = df[df['kfold'] == fold]
-    x_val = df_val.drop(['kfold', 'target'], axis=1).values
-    y_val = df_val.target.values
-    y_pred = reg_model.predict(x_val)
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
-
-    print('Fold: %d, RMSE: %.5f, Training time: %s' % (fold, rmse, training_time))
+    if fold >= 0:
+        df_val = df[df['kfold'] == fold]
+        x_val = df_val.drop(['kfold', 'target'], axis=1).values
+        y_val = df_val.target.values
+        y_pred = reg_model.predict(x_val)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        print('Fold: %d, RMSE: %.5f, Training time: %s' % (fold, rmse, training_time))
+    else:
+        rmse = 0.0
+        print('All data, Training time: %s' % training_time)
+        fold = 'all'
 
     model_filename = model + '_fold_' + str(fold) + '.pickle'
     model_path = os.path.join(config.MODEL_OUTPUT, model_filename)
     with open(model_path, 'wb') as f:
         pickle.dump(reg_model, f)
+    
+    return rmse
 
 
 if __name__ == '__main__':
@@ -45,5 +52,9 @@ if __name__ == '__main__':
     parser.add_argument('--folds', type=int, default=5)
     args = parser.parse_args()
 
+    errors = []
     for fold in range(args.folds):
-        run(args.model, fold)
+        rmse = run(args.model, fold)
+        errors.append(rmse)
+    print('Average RMSE over %d folds: %.5f' % (args.folds, np.mean(errors)))
+    run(args.model, -1)
